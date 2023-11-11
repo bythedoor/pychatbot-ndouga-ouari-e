ea742bc619d511aa2bfcbd660026aa44ef59a18a
kim ⋆.ೃ࿔*:･ <77552266+bythedoor@users.noreply.github.com>
2023-11-12 00:24:15 +0100

    Maj tous les fichiers

--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
 import os
 
-def file_names():
-    cwd = os.getcwd()
-    dir_list = os.listdir(cwd)
-    return dir_list
 
+""" 
+Cette fonction fait la même chose que list_of_files,
+sauf qu'elle retourne le nom des fichiers
+d'un repertoire donné, peu importe leur type.
+"""
+
+def nom_fichiers(directory):
+    dir_list = os.listdir(directory)
+    return dir_list
 
 def list_of_files(directory, extension):
     files_names = []
@@ -12,3 +17,48 @@ def list_of_files(directory, extension):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
+
+
+"""
+Cette fonction associe à chaque nom de président son prénom
+"""
+def presidents(last_names):
+    list_presidents = {}
+    first_names = []
+
+    cpt = 0
+    for i in first_names:
+        list_presidents[i] = last_names[cpt]
+        cpt += 1
+
+"""
+Cette fonction affiche les noms des différents présidents
+"""
+def print_names(names):
+    cpt = 0
+
+    for i in names:
+        for j in range(len(names)):
+            if names[j] == i:
+                cpt += 1
+        if cpt > 1:
+            print(i, end=" ")
+
+    print()
+
+"""
+Cette fonction renvoie un fichier en minuscules
+"""
+def lowercase(text, clean_text):
+    with open(text, "r") as f1, open(clean_text, "w") as f2:
+        for ligne in f1:
+            for i in ligne:
+                if ord(i) >= 65 and ord(i) <= 90:
+                    f2.write(chr(ord(i + 32)))
+                elif i == "\n":
+                    f2.write("\n")
+                else:
+                    f2.write(i)
+
+    return(clean_text)
+
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# Programme principal du projet
+#Programme principal du projet
+
+from functions import *
+
+if __name__ == "__main__":
+    names = nom_fichiers("speeches")
+    print_names(names)
