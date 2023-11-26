c22201054fcfddd8b62fb69cc32088e43cca0562
Camelouch <93676537+Camelouch@users.noreply.github.com>
2023-11-26 22:52:37 +0100

    - fonction chirac terminé

--- functions.py
+++ functions.py
@@ -255,8 +255,33 @@ def null_tf_idf():
 def high_tf_idf():
     return L
 
-def chirac():
-    return mot
+
+
+def chirac(directory):
+    # variable qui contiendra le/les mots les plus répété(s)
+    mot_plus_repete = ""
+    # Variable qui stoquera l'occurence du/des mots le/les plus utilisé(s)
+    occ = 0
+
+    # Boucle qui parcours les documents dans lesquel le mot "Chirac" apparait
+    for doc in os.listdir(directory):
+        if doc.endswith(".txt") and "Chirac" in doc:
+            with open(os.path.join(directory, doc), "r") as f:
+                # appel du dicyionnaire TF du document en question (doc)
+                d_tf = tf(doc, directory)
+
+                # Bouble qui parcours le dictionnaire TF et qui compare les valeurs des occurences
+                for word, count in d_tf.items():
+                    if count > occ:
+                        occ = count
+                        mot_plus_repete = word
+                    elif count == occ:
+                        mot_plus_repete += ", " + word
+
+    # Création de la phrase réponse
+    phrase = "Le(s) mot(s) le(s) plus répété(s) est/sont '" + mot_plus_repete + "'"
+
+    return phrase
 
 def nation():
     return
