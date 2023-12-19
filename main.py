d7baa2de9add6c7a14c2b99f95ee9e8fe3186756
Camelouch <93676537+Camelouch@users.noreply.github.com>
2023-12-19 09:34:25 +0100

    - fonction punctuation transforme bien tous les fichier sans ponctuation --> fct fini  - problème avec la fonction IDF, elle renvoie des negatifs, à revoir

--- main.py
+++ main.py
@@ -13,40 +13,101 @@ def main():  # Fonction principale, execute le programme entier
     print("3 - Les mots les plus répétés par Chirac")
     print("4 - Liste des présidents mentionant le mot 'Nation', et celui qui le dit plus souvent")
     print("5 - Le premier président à parler d'écologie ou du climat")
-    print("6 - Liste des mots que tous les présidents ont mentionné")
+    #print("6 - Liste des mots que tous les présidents ont mentionné")
 
     # L'utilisateur choisit une des fonctionnalités, numérotées de 1 à 6
-    n = -1
+    n = 0
     while n < 0 or n > 6:
         n = int(input("Choississez un nombre >> "))
 
     # Un programme s'exécute en fonction de la fonctionnalité choisie
-    if n == 1:
-        p = null_tf_idf("speeches")
+    if n == 1:  # remplacer print("0") par fonctions
+        p = null_tf_idf("cleaned")
         if len(p) == 0:
             print("Il n'y a pas de mots au score TF-IDF nul")
         else:
             print("Les mots non importants sont: ")
-            for i in range(len(p)):
-                print(p[i], end=" ")
+            for i in p:
+                print(p[i], end=" ,")
     elif n == 2:
         p = high_tf_idf(("cleaned"))
-        print("Les mots importants sont: ")
-        for i in p:
-            print(i, end=" - ")
-
+        print("Les mots importants sont: ", end=" ")
+        for i in range(len(p)):
+            print(p[i], end=" ,")
     elif n == 3:
-        print(chirac("speeches"))
+        print(chirac("cleaned"))
 
-    elif n == 4:
-        print(nation())
+    elif n == 4: # liste des présidents qui on dit le mot 'nation'
+        p = mot_dit('nation')
+        print("Les présidents qui ont dit le mot 'nation' sont", end=" ")
+        cpt = 0
+        for i in p:
+            if cpt == len(p):
+                print("et ", i, end=".")
+            else:
+                print(i, end=", ")
+            cpt += 1
 
     elif n == 5:
-        print("0")
-    elif n == 6:
-        print("0")
+        ecologie = mot_dit('ecologie')
+        climat = mot_dit('climat')
+        print("Le président qui ont dit le parle de climat et d'écologie est", end=" ")
+        for i in ecologie:
+            climat.add(i)
+        final = set(climat)
 
-t = token_question("Bonjour messieurs ?")
-print(t)
-print(intersection(t, "cleaned"))
+        cpt = 0
+        for i in final:
+            if cpt == len(final):
+                print("et ", i, end=".")
+            else:
+                print(i, end=", ")
+            cpt += 1
+    #elif n == 6:
+    #    print("0")
 
+
+
+
+if __name__ == "__main__":
+    main() # on execute la fonction principale
+
+    # tests des fonctions
+    """
+    n = "speeches"
+    print(list_last_names(n))
+    print(presidents(n))
+    """
+    """
+    print(score_tf_idf('Nomination_Chirac1.txt', 'speeches'))
+    # print(null_tf_idf())#))
+    # print(idf('speeches'))
+    # print(matrix_tf_idf('speeches'))
+    # print(tf('Nomination_Chirac1.txt', 'speeches'))
+    print(chirac('speeches'))
+    # print(print_names('speeches'))
+    print(presidents('speeches'))
+    print(list_last_names('speeches'))
+    print(list_of_files('speeches', '.txt'))
+    print(lowercase('speeches'))
+    print(punctuation('Nomination_Chirac1.txt', 'cleaned/clean_Chirac.txt'))
+    """
+    #print(lowercase('speeches'))
+    #print(punctuation('cleaned'))
+    #print(idf('cleaned'))
+    #print(matrix_tf_idf('cleaned'))
+    #print(null_tf_idf('cleaned'))
+    #jf
+    #print(chirac('cleaned'))
+    #print(high_tf_idf('cleaned'))
+    #print(mot_dit("nation"))
+    #print(list_last_names('cleaned'))
+    #print(presidents('cleaned'))
+    #print(print_names('speeches'))
+    #print(tf_idf_token('de quoi ca parle'))
+    #print(create_tfidf_matrix_optimized('cleaned'))
+    #print(score_tf_idf_text('Nomination_Chirac1_clean.txt', 'cleaned'))
+    print(matrix_tf_idf_2('cleaned'))
+    print(tf_idf_token('la responsabilité qu ils m ont confiée est un honneur dont je mesure la gravité'))
+    print(pertinent_document(matrix_tf_idf_2('cleaned'), tf_idf_token('la responsabilité qu ils m ont confiée est un honneur dont je mesure la gravité'), name_files('cleaned')))
+    print(generate_answer("responsabilité qu ils m ont confiée est un honneur dont je mesure la gravité"))
